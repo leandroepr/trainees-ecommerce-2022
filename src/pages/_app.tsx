d630fb6f3322d03538bd229012d288151022f359
Leandro Reis <leandro.e.reis@gmail.com>
2022-07-14 18:12:34 -0300

    chore: add react-query to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from "next/app";
+import React from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import "styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
