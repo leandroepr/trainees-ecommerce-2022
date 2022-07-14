829d391fecf90314c36f96f24c923aa5e861a8c4
Leandro Reis <leandro.e.reis@gmail.com>
2022-07-14 17:44:12 -0300

    chore: add tailwindcss to project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="">Hello World!</div>
-  )
-}
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
-export default Home
+export default Home;
