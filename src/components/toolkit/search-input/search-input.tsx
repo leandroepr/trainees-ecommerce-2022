c05404fd48f5bf5e370665ec210d37c2f9a13e5d
Leandro Reis <leandro.e.reis@gmail.com>
2022-07-15 08:59:01 -0300

    feat: create searchInput component

---
+++ src/components/toolkit/search-input/search-input.tsx
@@ -0,0 +1,62 @@
+import { SearchIcon } from "@heroicons/react/outline";
+import { classNames } from "helpers/class-names";
+import React from "react";
+
+export interface SearchInputProps {
+  value?: string;
+  placeholder?: string;
+  id?: string;
+  name?: string;
+  label?: string;
+  className?: string;
+  // pending?: boolean;
+  onChange?: (value: string) => void;
+  onSearch?: (value: string) => void;
+}
+const SearchInput: React.FC<SearchInputProps> = ({
+  onChange,
+  onSearch,
+  className,
+  ...inputProps
+}) => {
+  const handleOnSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onSearch?.(internalValue);
+    }
+  };
+  const handleOnClickToSearch = () => {
+    onSearch?.(internalValue);
+  };
+  const [internalValue, setInternalValue] = React.useState("");
+  React.useEffect(() => {
+    onChange?.(internalValue);
+  }, [internalValue, onChange]);
+
+  return (
+    <div
+      className={classNames(
+        "relative focus-within:text-sky-500 text-gray-400",
+        className
+      )}
+    >
+      <input
+        type="search"
+        value={internalValue}
+        onChange={(event) => setInternalValue(event.target.value)}
+        className={classNames(
+          "shadow-sm focus:ring-sky-500 focus:border-sky-500 block w-full sm:text-sm border-gray-300 rounded-md placeholder:text-gray-400 text-gray-700 pr-10"
+        )}
+        onKeyUp={handleOnSearch}
+        {...inputProps}
+      />
+      <button
+        className="absolute right-0 z-10 inset-y-0 flex items-center justify-center shrink-0 border-l my-2 px-2 active:opacity-75 hover:text-opacity-90 "
+        onClick={handleOnClickToSearch}
+      >
+        <SearchIcon className="w-4 h-4 " />
+      </button>
+    </div>
+  );
+};
+
+export default SearchInput;
