34d1a4e377488638aaabb782bc3c04e2cc23ad43
Leandro Reis <leandro.e.reis@gmail.com>
2022-07-15 08:57:55 -0300

    feat: create className helper

---
+++ src/helpers/class-names.ts
@@ -0,0 +1,3 @@
+type className = string | null | undefined | (() => string);
+export const classNames = (...classNames: className[]): string =>
+  classNames.filter(Boolean).join(" ").replace(/\s\s+/g, " ").trim();
