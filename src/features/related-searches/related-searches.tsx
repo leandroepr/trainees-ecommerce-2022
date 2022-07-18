e15fd45e80e9c274d4288c70e419eb7adda362ce
fsvboas <fsvboas.dev@gmail.com>
2022-07-18 09:37:50 -0300

    fix: add react import

--- src/features/related-searches/related-searches.tsx
+++ src/features/related-searches/related-searches.tsx
@@ -1,18 +1,19 @@
-import { classNames } from "helpers/class-names";
-import Link from "next/link";
+import { classNames } from 'helpers/class-names'
+import Link from 'next/link'
+import React from 'react'
 
 const RelatedSearches: React.FC = () => {
   const relatedList = [
-    "roupas femininas",
-    "camisas masculinas",
-    "body feminino",
-    "cropped",
-    "conjuntos femininos",
-  ];
+    'roupas femininas',
+    'camisas masculinas',
+    'body feminino',
+    'cropped',
+    'conjuntos femininos'
+  ]
 
   return (
     <div className="flex">
-      <h3 className={classNames("font-bold text-gray-700")}>
+      <h3 className={classNames('font-bold text-gray-700')}>
         Buscas relacionadas:
       </h3>
       {relatedList.map((relatedItem, i) => (
@@ -20,19 +21,19 @@ const RelatedSearches: React.FC = () => {
           <div>
             <a
               className={classNames(
-                "ml-2 cursor-pointer text-gray-500 font-normal"
+                'ml-2 cursor-pointer text-gray-500 font-normal'
               )}
             >
               {relatedItem}
             </a>
-            <span className={classNames("ml-2")}>
-              {i < relatedList.length - 1 ? "-" : null}
+            <span className={classNames('ml-2')}>
+              {i < relatedList.length - 1 ? '-' : null}
             </span>
           </div>
         </Link>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default RelatedSearches;
+export default RelatedSearches
