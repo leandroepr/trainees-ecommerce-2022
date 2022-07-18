2a825af94cb02d34f880ae1da99ea0be6c525ced
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-07-18 14:04:19 -0300

    feat: add button and quantity of cards to show

--- src/features/product/product-list/category-products-list.tsx
+++ src/features/product/product-list/category-products-list.tsx
@@ -1,3 +1,4 @@
+import { ChevronRightIcon } from '@heroicons/react/outline'
 import Column from 'components/toolkit/column'
 import Row from 'components/toolkit/row'
 import Text from 'components/toolkit/text'
@@ -10,13 +11,22 @@ export interface CategoryProductsListProps {
   className?: string
   categoryName?: string
   categoryPageLink?: string
+  cardsQuantity?: number
 }
 const CategoryProductsList: React.FC<CategoryProductsListProps> = ({
   children,
   className,
   categoryName = '',
   categoryPageLink = '/',
+  cardsQuantity = 5,
 }) => {
+  const sliceCardArray = ({
+    cards,
+    quantity,
+  }: {
+    cards: JSX.Element[] | JSX.Element
+    quantity: number
+  }) => (Array.isArray(cards) ? cards.slice(0, quantity) : cards)
   return (
     <Column className={classNames('space-y-5', className)}>
       <Row className="items-baseline space-x-4">
@@ -27,7 +37,16 @@ const CategoryProductsList: React.FC<CategoryProductsListProps> = ({
           </a>
         </Link>
       </Row>
-      <Row className="space-x-4">{children}</Row>
+      <Row className="space-x-4 items-center">
+        {sliceCardArray({ cards: children || [], quantity: cardsQuantity })}
+        <Link href={categoryPageLink}>
+          <a>
+            <span className="w-16 h-16 p-2 bg-white rounded-full shadow-md flex items-center justify-center -translate-x-12">
+              <ChevronRightIcon className="w-6, h-6 text-blue-500 stroke-[3px]" />
+            </span>
+          </a>
+        </Link>
+      </Row>
     </Column>
   )
 }
