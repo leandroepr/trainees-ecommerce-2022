2bc86ed376b6ac5a0d69f0e76acf3b1e09080195
fsvboas <fsvboas.dev@gmail.com>
2022-07-18 11:26:54 -0300

    chore: standardize colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ module.exports = {
     './src/features/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: 'rgb(var(--black)  / 1 )',
+        white: 'rgb(var(--white) / 1 )',
+        grey: 'rgb(var(--grey) / 1 )',
+        lightGrey: 'rgb(var(--light-grey)  / 1 )',
+        yellow: 'rgb(var(--yellow) / 1 )',
+        blue: 'rgb(var(--blue) / 1 )',
+        darkBlue: 'rgb(var(--dark-blue) / 1 )',
+        thinBlue: 'rgb(var(--thin-blue) / 1 )',
+        green: 'rgb(var(--green) / 1 )',
+        red: 'rgb(var(--red) / 1 )',
+      }
+    },
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
 }
