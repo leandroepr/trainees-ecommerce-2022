e7c40f4c8625fae24573256d95770215b8304e9c
thiagobachiega <thiagobachiega1@gmail.com>
2022-07-18 11:07:57 -0300

    bugfix: exclude redundant product screen component

--- src/components/screens/products/products-screen.tsx
+++ src/components/screens/products/products-screen.tsx
@@ -1,9 +1,7 @@
 import {
-  Body,
-  Footer,
-  Header,
   PublicLayout,
 } from 'components/templates/public-layout'
+import DefaultPublicLayout from 'components/templates/public-layout/default-public-layout'
 import Grid from 'components/toolkit/grid/grid'
 import Placeholder from 'components/toolkit/placeholder/placeholder'
 import Row from 'components/toolkit/row/row'
@@ -11,7 +9,7 @@ import Row from 'components/toolkit/row/row'
 
 const ProductsScreen: React.FC = () => {
   return (
-    <PublicLayout>
+    <DefaultPublicLayout>
         <Placeholder height="50" width="800" text="Buscas Relacionadas" />
         <Row className="flex-row justify-between">
           <Placeholder height="600" width="300" text="Menu Lateral" />
@@ -24,7 +22,7 @@ const ProductsScreen: React.FC = () => {
             <Placeholder height="500" width="300" text="Card" />
           </Grid>
         </Row>
-    </PublicLayout>
+    </DefaultPublicLayout>
   )
 }
 
