09a3830a524871f31b565ce8f66a81457246105e
thiagobachiega <thiagobachiega1@gmail.com>
2022-07-18 09:30:01 -0300

    refactor: adjust code to match new public layout component

--- src/components/screens/products/products-screen.tsx
+++ src/components/screens/products/products-screen.tsx
@@ -12,10 +12,6 @@ import Row from 'components/toolkit/row/row'
 const ProductsScreen: React.FC = () => {
   return (
     <PublicLayout>
-      <Header className="py-4">
-        <span>header</span>
-      </Header>
-      <Body className="py-2">
         <Placeholder height="50" width="800" text="Buscas Relacionadas" />
         <Row className="flex-row justify-between">
           <Placeholder height="600" width="300" text="Menu Lateral" />
@@ -28,10 +24,6 @@ const ProductsScreen: React.FC = () => {
             <Placeholder height="500" width="300" text="Card" />
           </Grid>
         </Row>
-      </Body>
-      <Footer className="text-center">
-        <span>footer</span>
-      </Footer>
     </PublicLayout>
   )
 }
