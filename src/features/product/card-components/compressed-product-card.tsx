e31b8c78ae443befb0d44dea3ff887c335c2dcf1
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-07-18 09:10:37 -0300

    feature: finished compressed and expanded cards

--- src/features/product/card-components/compressed-product-card.tsx
+++ src/features/product/card-components/compressed-product-card.tsx
@@ -1,26 +1,27 @@
 import Column from 'components/toolkit/column'
-import { ImageComponent } from 'components/toolkit/image'
+import ImageComponent from 'components/toolkit/image'
 import Row from 'components/toolkit/row'
 import Text from 'components/toolkit/text'
 import { classNames } from 'helpers/class-names'
 import Link from 'next/link'
+import { ProductInstallmentType } from 'types/card-component-props'
 
 export interface CompressedProductCardProps {
   className?: string
   productPrice?: number
-  installment?: { quantity: number; value: number }
-  image?: { src?: string; alt?: string }
+  productInstallment?: ProductInstallmentType
+  productImage?: { src?: string; alt?: string }
   productLink: string
 }
 
 const CompressedProductCard: React.FC<CompressedProductCardProps> = ({
   className,
   productPrice = 0,
-  installment = {
+  productInstallment = {
     quantity: 0,
     value: 0,
   },
-  image = {
+  productImage = {
     src: '',
     alt: '',
   },
@@ -31,44 +32,42 @@ const CompressedProductCard: React.FC<CompressedProductCardProps> = ({
     const cents = ('0' + Math.round((integerPrice % 1) * 100)).slice(-2)
     return { currency, cents }
   }
-  const formatInstallment = ({ quantity, value }: typeof installment) =>
+  const formatInstallment = ({ quantity, value }: ProductInstallmentType) =>
     `em ${quantity}x de ${value.toLocaleString('pt-BR', {
       style: 'currency',
       currency: 'BRL',
     })}`
 
-  const splittedProductPrice = splitProductPrice(productPrice)
-  const formattedInstallment = formatInstallment(installment)
+  const { cents, currency } = splitProductPrice(productPrice)
+  const formattedInstallment = formatInstallment(productInstallment)
   const showingCurrency = 'R$'
 
   return (
     <Link href={productLink}>
       <Column
         className={classNames(
-          'bg-white shadow border rounded-md overflow-hidden cursor-pointer',
+          'bg-white shadow border rounded-md overflow-hidden cursor-pointer hover:shadow-md group',
           className
         )}
       >
-        <Column className="items-center justify-center">
+        <Column className="justify-center aspect-square overflow-hidden">
           <ImageComponent
-            src={image.src}
-            alt={image.alt}
+            src={productImage.src}
+            alt={productImage.alt}
             noSrcAlt="https://m.media-amazon.com/images/I/3157LdLqwVL._AC_SY395_.jpg"
           />
         </Column>
-        <Column className="border-t flex-grow p-5">
+        <Column className="border-t flex-grow p-5 group-active:bg-gray-100">
           <Row>
             <Text textType="cardCurrency" className="mr-1">
               {showingCurrency}
             </Text>
             <Text textType="cardCurrency" className="mr-[2px]">
-              {splittedProductPrice.currency}
+              {currency}
             </Text>
-            {Number(splittedProductPrice.cents) > 0 && (
-              <Text textType="cardCents">{splittedProductPrice.cents}</Text>
-            )}
+            {Number(cents) > 0 && <Text textType="cardCents">{cents}</Text>}
           </Row>
-          {installment.value > 0 && installment.quantity > 0 && (
+          {productInstallment.value > 0 && productInstallment.quantity > 0 && (
             <Text textType="cardInstallment">{formattedInstallment}</Text>
           )}
         </Column>
--- src/features/product/card-components/expanded-product-card.tsx
+++ src/features/product/card-components/expanded-product-card.tsx
@@ -1,16 +1,22 @@
 import Column from 'components/toolkit/column'
-import { ImageComponent } from 'components/toolkit/image'
+import ImageComponent from 'components/toolkit/image'
 import Row from 'components/toolkit/row'
+import Tag from 'components/toolkit/tag'
+import { tagStyleType } from 'components/toolkit/tag/tag'
 import Text from 'components/toolkit/text'
 import { classNames } from 'helpers/class-names'
 import Link from 'next/link'
+import {
+  ProductImageType,
+  ProductInstallmentType,
+} from 'types/card-component-props'
 
 export interface ExpandedProductCardProps {
   className?: string
   productPrice?: number
-  installment?: { quantity: number; value: number }
-  image?: { src?: string; alt?: string }
-  tags?: string[]
+  productInstallment?: ProductInstallmentType
+  productImage?: ProductImageType
+  productTags?: { style: tagStyleType; content: string }[]
   productName?: string
   productLink: string
 }
@@ -18,15 +24,15 @@ export interface ExpandedProductCardProps {
 const ExpandedProductCard: React.FC<ExpandedProductCardProps> = ({
   className,
   productPrice = 0,
-  installment = {
+  productInstallment = {
     quantity: 0,
     value: 0,
   },
-  image = {
+  productImage = {
     src: '',
     alt: '',
   },
-  tags = [],
+  productTags: tags = [],
   productName = '',
   productLink = '/',
 }) => {
@@ -35,61 +41,63 @@ const ExpandedProductCard: React.FC<ExpandedProductCardProps> = ({
     const cents = ('0' + Math.round((integerPrice % 1) * 100)).slice(-2)
     return { currency, cents }
   }
-  const formatInstallment = ({ quantity, value }: typeof installment) =>
+  const formatInstallment = ({ quantity, value }: ProductInstallmentType) =>
     `em ${quantity}x de ${value.toLocaleString('pt-BR', {
       style: 'currency',
       currency: 'BRL',
     })}`
 
   const splittedProductPrice = splitProductPrice(productPrice)
-  const formattedInstallment = formatInstallment(installment)
+  const formattedInstallment = formatInstallment(productInstallment)
   const showingCurrency = 'R$'
 
   return (
     <Link href={productLink}>
       <Column
         className={classNames(
-          'bg-white shadow border rounded-md overflow-hidden cursor-pointer',
+          'bg-white shadow border rounded-md cursor-pointer hover:shadow-md group',
           className
         )}
       >
-        <Column className="items-center justify-center">
+        <Column className="justify-center aspect-square">
           <ImageComponent
-            src={image.src}
-            alt={image.alt}
+            src={productImage.src}
+            alt={productImage.alt}
             noSrcAlt="https://m.media-amazon.com/images/I/3157LdLqwVL._AC_SY395_.jpg"
           />
         </Column>
-        <Column className="border-t flex-grow p-5">
-          <Row>
-            <Text textType="cardCurrency" className="mr-1">
-              {showingCurrency}
-            </Text>
-            <Text textType="cardCurrency" className="mr-[2px]">
-              {splittedProductPrice.currency}
-            </Text>
-            {Number(splittedProductPrice.cents) > 0 && (
-              <Text textType="cardCents">{splittedProductPrice.cents}</Text>
-            )}
-          </Row>
-          {installment.value > 0 && installment.quantity > 0 && (
-            <Text textType="expandedCardInstallment">
-              {formattedInstallment}
-            </Text>
-          )}
+        <Column className="border-t flex-grow p-5 space-y-3 group-active:bg-gray-100">
+          <Column>
+            <Row>
+              <Text textType="expandedCardCurrency" className="mr-1">
+                {showingCurrency}
+              </Text>
+              <Text textType="expandedCardCurrency" className="mr-[2px]">
+                {splittedProductPrice.currency}
+              </Text>
+              {Number(splittedProductPrice.cents) > 0 && (
+                <Text textType="expandedCardCents">
+                  {splittedProductPrice.cents}
+                </Text>
+              )}
+            </Row>
+            {productInstallment.value > 0 &&
+              productInstallment.quantity > 0 && (
+                <Text textType="expandedCardInstallment">
+                  {formattedInstallment}
+                </Text>
+              )}
+          </Column>
           {tags.length > 0 && (
             <Row className="flex-wrap gap-1 max-h-[64px] overflow-hidden">
               {tags.map((tag) => (
-                <span
-                  className="w-fit px-1 py-[1px] bg-sky-100 rounded-sm text-xs text-sky-600 truncate 6"
-                  key={tag}
-                >
-                  {tag}
-                </span>
+                <Tag style={tag.style} key={tag.content}>
+                  {tag.content}
+                </Tag>
               ))}
             </Row>
           )}
-          <Text textType="productName" className="break-words line-clamp-2">
+          <Text textType="productName" className="break-words line-clamp-3">
             {productName}
           </Text>
         </Column>
