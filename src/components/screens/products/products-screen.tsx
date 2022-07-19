6962cdd29660662ed1a7d870513721c99fc65870
thiagobachiega <thiagobachiega1@gmail.com>
2022-07-19 11:54:00 -0300

    feature/category-page-componentes-integration

--- src/components/screens/products/products-screen.tsx
+++ src/components/screens/products/products-screen.tsx
@@ -1,24 +1,17 @@
 import CategoryLateralMenuLayout from 'components/category-lateral-menu-layout/category-lateral-menu-layout'
 import DefaultPublicLayout from 'components/templates/public-layout/default-public-layout'
-import Grid from 'components/toolkit/grid/grid'
-import Placeholder from 'components/toolkit/placeholder/placeholder'
-import Row from 'components/toolkit/row/row'
+import { Row } from 'components/toolkit'
+import { ProductsList } from 'features/category'
+import RelatedSearches from 'features/related-searches/related-searches'
 
 
 const ProductsScreen: React.FC = () => {
   return (
     <DefaultPublicLayout>
-        <Placeholder height="50" width="800" text="Buscas Relacionadas" />
-        <Row className="flex-row justify-between">
+          <RelatedSearches />
+        <Row className='justify-between mb-2'>
           <CategoryLateralMenuLayout/>
-          <Grid>
-            <Placeholder height="500" width="300" text="Card" />
-            <Placeholder height="500" width="300" text="Card" />
-            <Placeholder height="500" width="300" text="Card" />
-            <Placeholder height="500" width="300" text="Card" />
-            <Placeholder height="500" width="300" text="Card" />
-            <Placeholder height="500" width="300" text="Card" />
-          </Grid>
+          <ProductsList/>
         </Row>
     </DefaultPublicLayout>
   )
