29986d05cf34801134f22b00cb7f4640d42aa875
fsvboas <fsvboas.dev@gmail.com>
2022-07-19 13:47:39 -0300

    feat: create use-toast-notification hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { ToastProvider } from 'react-toast-notifications'
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient())
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
+    <ToastProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+    </ToastProvider>
   )
 }
 
