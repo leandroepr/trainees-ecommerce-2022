6119a0d72aad51590a13b953f8b665f7cbdb74a5
fsvboas <fsvboas.dev@gmail.com>
2022-07-20 18:54:05 -0300

    refactor: remove isLoading from products-list

--- src/features/category/products-list/products-list.tsx
+++ src/features/category/products-list/products-list.tsx
@@ -6,10 +6,10 @@ import { classNames } from "helpers/class-names";
 import React from "react";
 
 const ProductsList: React.FC = () => {
-  const { isLoading, data: products } = useGetAllProducts()
+  const { data: products } = useGetAllProducts()
+
   return (
     <Row className={classNames('max-w-screen-lg')}>
-      {isLoading && <div>Carregando...</div>}
       <Grid
         className={classNames(
           'grid gap-3 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'
