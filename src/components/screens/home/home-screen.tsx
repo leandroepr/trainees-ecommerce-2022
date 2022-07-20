d1e249433ed19b7a2f004cc9158e2ef667ea2fd3
fsvboas <fsvboas.dev@gmail.com>
2022-07-20 18:53:31 -0300

    feature: add spinner component to pages

--- src/components/screens/home/home-screen.tsx
+++ src/components/screens/home/home-screen.tsx
@@ -1,6 +1,9 @@
 import { useGetAllCategories } from 'api-hook/category/use-get-all-categories'
 import DefaultPublicLayout from 'components/templates/public-layout/default-public-layout'
+import { Column } from 'components/toolkit'
+import Spinner from 'components/toolkit/spinner/spinner'
 import CategoryProductsList from 'features/product/product-list/category-products-list'
+import { classNames } from 'helpers/class-names'
 import React from 'react'
 
 // export interface HomeScreenProps {}
@@ -9,7 +12,7 @@ const HomeScreen: React.FC = () => {
 
   return (
     <DefaultPublicLayout className="space-y-12 py-12" title="Home">
-      {isLoading && <div>Carregando...</div>}
+      {isLoading && <Column className={classNames('items-center')}><Spinner color="black" size={100} />Carregando...</Column>}
       {categories?.map((category) => (
         <CategoryProductsList
           key={category.name}
