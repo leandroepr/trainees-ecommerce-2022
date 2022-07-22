f0bb3cbf76c9d135a81a354168968b2144da97cb
Leandro Reis <leandro.e.reis@gmail.com>
2022-07-22 14:53:52 -0300

    refactor: remove custom padding from tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,15 +20,6 @@ module.exports = {
       fontFamily: {
         custom: ['ubuntu-mono', 'Ubuntu', 'sans-serif'],
       },
-       container: {
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
-      }
     },
     plugins: [
       require('@tailwindcss/forms'),
