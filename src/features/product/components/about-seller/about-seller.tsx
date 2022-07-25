7b9478e9762ea6c85048848c4dd20cc4d587a99a
fsvboas <fsvboas.dev@gmail.com>
2022-07-25 16:49:48 -0300

    refactor: add text-component to related-searches, about-seller and warranty components

--- src/features/product/components/about-seller/about-seller.tsx
+++ src/features/product/components/about-seller/about-seller.tsx
@@ -1,5 +1,5 @@
 import { ChatAlt2Icon, ClockIcon, LocationMarkerIcon } from '@heroicons/react/outline'
-import { Column } from 'components/toolkit'
+import { Column, Link, Text } from 'components/toolkit'
 import Row from 'components/toolkit/row/row'
 import React from 'react'
 
@@ -7,15 +7,15 @@ const AboutSeller: React.FC = () => {
   return (
     <Column className='max-w-md'>
       <Row className='py-4'>
-        <h3 className='font-bold'>
+        <Text as='h2' className='font-bold'>
           Informações sobre o vendedor
-        </h3>
+        </Text>
       </Row>
       <Row className='pb-8 items-center'>
         <Column className='pr-2'><LocationMarkerIcon className="w-8 h-8" /></Column>
         <Column>
-          <h4 className='font-bold'>Localização</h4>
-          <span className='font-semibold text-dark/50'>São Paulo, São Paulo</span>
+          <Text as='h3' className='font-bold'>Localização</Text>
+          <Text className='font-semibold text-dark/50'>São Paulo, São Paulo</Text>
         </Column>
       </Row>
       <Row className='pb-8 justify-center'>
@@ -29,20 +29,20 @@ const AboutSeller: React.FC = () => {
       </Row>
       <Row className='pb-6'>
         <Column className='p-2 items-center justify-center text-center border-r-2 border-dark/10'>
-          <h1 className='font-bold text-xl'>561</h1>
-          <span className='font-semibold text-sm'>vendas nos últimos 4 meses</span>
+          <Text as='h3' className='font-bold text-xl'>561</Text>
+          <Text className='font-semibold text-sm'>vendas nos últimos 4 meses</Text>
         </Column>
         <Column className='p-2 items-center justify-center text-center border-r-2 border-dark/10'>
-          <ChatAlt2Icon className='w-8 h-8'/>
-          <span className='font-semibold text-sm'>Presta um bom atendimento</span>
+          <ChatAlt2Icon className='w-8 h-8' />
+          <Text className='font-semibold text-sm'>Presta um bom atendimento</Text>
         </Column>
         <Column className='p-2 items-center justify-center text-center'>
-          <ClockIcon className='w-8 h-8'/>
-          <span className='font-semibold text-sm'>Entrega os produtos dentro do prazo</span>
+          <ClockIcon className='w-8 h-8' />
+          <Text className='font-semibold text-sm'>Entrega os produtos dentro do prazo</Text>
         </Column>
       </Row>
       <Row>
-        <a className='cursor-pointer text-info font-bold'>Ver mais dados do vendedor</a>
+        <Link href='#' className='cursor-pointer text-info font-bold'>Ver mais dados do vendedor</Link>
       </Row>
     </Column>
   )
