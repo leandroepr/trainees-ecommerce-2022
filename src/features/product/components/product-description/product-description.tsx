0cb2d235c1f507958888d6f87d10b59bbd88e579
fsvboas <fsvboas.dev@gmail.com>
2022-07-25 10:06:28 -0300

    feat: create product description component

---
+++ src/features/product/components/product-description/product-description.tsx
@@ -0,0 +1,16 @@
+import { Text } from 'components/toolkit'
+import Column from 'components/toolkit/column/column'
+
+const ProductDescription: React.FC = () => {
+  return (
+    <div>
+      <Column>
+        <Text className='pb-4 font-bold text-xl' as='h3'>Descrição</Text>
+        <Text className='font-medium text-dark/60 w-full' as='p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+      </Column>
+    </div>
+  )
+}
+
+export default ProductDescription
