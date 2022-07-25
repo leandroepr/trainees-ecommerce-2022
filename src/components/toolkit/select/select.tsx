40fd9149eee6eb3441bd235eebfdbc8ac30c7e03
Álvaro Palma <palma.alvaro@outlook.com>
2022-07-25 12:20:31 -0300

    fix: add select, label and options class names

--- src/components/toolkit/select/select.tsx
+++ src/components/toolkit/select/select.tsx
@@ -3,16 +3,39 @@ type Value = string | number
 // https://stackoverflow.com/questions/66738941/how-to-type-a-custom-react-select-component-using-typescript
 interface SelectProps {
   id: string
+  label?: string
   value: Value
   onChange: (event: string) => void
   options: readonly Value[]
+  optionsClassName?: string
+  labelClassName?: string
+  selectClassName?: string
 }
-const Select = ({ id, value, onChange, options }: SelectProps) => {
+const Select = ({
+  id,
+  label,
+  value,
+  onChange,
+  options,
+  labelClassName,
+  optionsClassName,
+  selectClassName,
+}: SelectProps) => {
   return (
-    <label htmlFor={id}>
-      <select value={value} onChange={(e) => onChange(e.target.value)} id={id}>
+    <label htmlFor={id} className={labelClassName}>
+      {label}
+      <select
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        id={id}
+        className={selectClassName}
+      >
         {options.map((option) => (
-          <option value={option} key={options.indexOf(option)}>
+          <option
+            value={option}
+            key={options.indexOf(option)}
+            className={optionsClassName}
+          >
             {option}
           </option>
         ))}
