cb1d30b2b67f4748f3a7b5500884e336a3d4efa4
Álvaro Palma <palma.alvaro@outlook.com>
2022-07-25 11:42:47 -0300

    fix: select props

--- src/components/toolkit/select/select.tsx
+++ src/components/toolkit/select/select.tsx
@@ -2,15 +2,15 @@ type Value = string | number
 // type props made based in:
 // https://stackoverflow.com/questions/66738941/how-to-type-a-custom-react-select-component-using-typescript
 interface SelectProps {
-  label: string
+  id: string
   value: Value
-  onChange: (newValue: Value) => void
+  onChange: (event: string) => void
   options: readonly Value[]
 }
-const Select = ({ label, value, onChange, options }: SelectProps) => {
+const Select = ({ id, value, onChange, options }: SelectProps) => {
   return (
-    <label htmlFor={label}>
-      <select value={value} onChange={(e) => onChange(e.target.value)}>
+    <label htmlFor={id}>
+      <select value={value} onChange={(e) => onChange(e.target.value)} id={id}>
         {options.map((option) => (
           <option value={option} key={options.indexOf(option)}>
             {option}
