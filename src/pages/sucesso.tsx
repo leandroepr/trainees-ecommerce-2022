e526e0076c8cf20dd61e08b76aefdde3feb46043
fsvboas <fsvboas.dev@gmail.com>
2022-07-25 10:42:40 -0300

    feat: create success page

---
+++ src/pages/sucesso.tsx
@@ -0,0 +1,8 @@
+import { SuccessScreen } from 'components/screens/success'
+import type { NextPage } from 'next'
+
+const SuccessRoute: NextPage = () => {
+  return <SuccessScreen />
+}
+
+export default SuccessRoute
