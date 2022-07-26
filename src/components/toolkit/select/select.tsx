fefd4b8ad260d601b3a661848466b3d5d4a3d049
Álvaro Palma <palma.alvaro@outlook.com>
2022-07-26 10:27:26 -0300

    refactor: select for headless ui select

--- src/components/toolkit/select/select.tsx
+++ src/components/toolkit/select/select.tsx
@@ -1,46 +1,90 @@
-type Value = string | number
-// type props made based in:
-// https://stackoverflow.com/questions/66738941/how-to-type-a-custom-react-select-component-using-typescript
+import { Combobox, Transition } from '@headlessui/react'
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+import { Fragment, useState } from 'react'
+
 interface SelectProps {
-  id: string
   label?: string
-  value: Value
-  onChange: (event: string) => void
-  options: readonly Value[]
-  optionsClassName?: string
-  labelClassName?: string
-  selectClassName?: string
+  options: readonly string[]
 }
-const Select = ({
-  id,
-  label,
-  value,
-  onChange,
-  options,
-  labelClassName,
-  optionsClassName,
-  selectClassName,
-}: SelectProps) => {
+const Select = ({ options, label }: SelectProps) => {
+  const [selectedOption, setSelectedOption] = useState(options[0])
+  const [query, setQuery] = useState('')
+
+  const filteredOptions =
+    query === ''
+      ? options
+      : options.filter((option) => {
+          return option.toLowerCase().includes(query.toLowerCase())
+        })
+
   return (
-    <label htmlFor={id} className={labelClassName}>
-      {label}
-      <select
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        id={id}
-        className={selectClassName}
-      >
-        {options.map((option) => (
-          <option
-            value={option}
-            key={`option-${options.indexOf(option)}`}
-            className={optionsClassName}
+    <div className="top-16 w-72">
+      <Combobox value={selectedOption} onChange={setSelectedOption}>
+        {label && <Combobox.Label>{label}</Combobox.Label>}
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <SelectorIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
           >
-            {option}
-          </option>
-        ))}
-      </select>
-    </label>
+            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {filteredOptions.length === 0 && query !== '' ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredOptions.map((option) => (
+                  <Combobox.Option
+                    key={option}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                      }`
+                    }
+                    value={option}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {option}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
   )
 }
 
