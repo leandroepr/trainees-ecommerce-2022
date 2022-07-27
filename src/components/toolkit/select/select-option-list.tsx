ebad0e03f975b33008b3811bc44c036ba847f0a5
Dan Lima <danlima.tech@gmail.com>
2022-07-27 13:30:50 -0300

    style: change SelectOptionList z-index

--- src/components/toolkit/select/select-option-list.tsx
+++ src/components/toolkit/select/select-option-list.tsx
@@ -14,7 +14,7 @@ const SelectOptionList = ({
   return (
     <Combobox.Options
       className={classNames(
-        'absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm',
+        'z-30 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm',
         className
       )}
     >
