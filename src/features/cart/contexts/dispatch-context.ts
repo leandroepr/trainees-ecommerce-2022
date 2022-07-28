695a371863afac6eecb71fb1535592a2d1edb258
Álvaro Palma <palma.alvaro@outlook.com>
2022-07-28 10:19:08 -0300

    refactor: remove unused dispatch context

--- src/features/cart/contexts/dispatch-context.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { DispatchContextType } from '../types/dispatch-context-type'
-
-export const defaultDispatchContextValue: DispatchContextType = () => null
-
-export const DispatchContext = React.createContext(defaultDispatchContextValue)
