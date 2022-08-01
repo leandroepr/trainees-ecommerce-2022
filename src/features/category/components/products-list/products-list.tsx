70d588074cff1a41f0e5119b9776f550c61e947f
Dan Lima <danlima.tech@gmail.com>
2022-08-01 12:24:36 -0300

    feat: filter products by query using search input and links

--- src/features/category/components/products-list/products-list.tsx
+++ src/features/category/components/products-list/products-list.tsx
@@ -2,10 +2,20 @@ import { Link, Row } from 'components/toolkit'
 import { classNames } from 'core/helpers/class-names'
 import { ProductCard } from 'features/product'
 import { useGetAllProducts } from 'features/product/hooks/use-get-all-products'
+import { useRouter } from 'next/router'
 import React from 'react'
+
+type QueryType = {
+  filtro: string
+  categoria: string
+}
 
 const ProductsList: React.FC = () => {
   const { data: products } = useGetAllProducts()
+
+  const router = useRouter()
+
+  const { filtro, categoria } = router.query as QueryType
 
   return (
     <Row className={classNames('')}>
@@ -14,11 +24,17 @@ const ProductsList: React.FC = () => {
           'grid gap-3 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'
         )}
       >
-        {products?.map((product) => (
-          <Link key={product.slug} href={`/produtos/${product.slug}`}>
-            <ProductCard product={product} variant="detailed" />
-          </Link>
-        ))}
+        {products
+          ?.filter(
+            (product) =>
+              product.slug.includes(filtro) ||
+              product.categoryId.includes(categoria)
+          )
+          .map((product) => (
+            <Link key={product.slug} href={`/produtos/${product.slug}`}>
+              <ProductCard product={product} variant="detailed" />
+            </Link>
+          ))}
       </div>
     </Row>
   )
