4d00aa0c4c51b0a33ea68fd74a44f5609439f2fd
fsvboas <fsvboas.dev@gmail.com>
2022-08-02 19:21:27 -0300

    refactor: adjust input styles

--- src/components/toolkit/search-input.tsx
+++ src/components/toolkit/search-input.tsx
@@ -78,7 +78,7 @@ const SearchInput: React.FC<SearchInputProps> = ({
         value={internalValue}
         onChange={(event) => setInternalValue(event.target.value)}
         className={classNames(
-          'h-10 shadow-sm focus:ring-sky-500 focus:border-sky-500 block w-full sm:text-sm border-gray-300 rounded-md placeholder:text-gray-400 text-gray-700 pr-10 placeholder: pl-4'
+          'h-10 shadow-sm focus:ring-sky-500 focus:border-sky-500 block w-full sm:text-sm border-gray-300 rounded-md placeholder:text-gray-400 text-gray-700 pr-10 pl-4'
         )}
         onKeyUp={handleOnSearch}
         {...inputProps}
